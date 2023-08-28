a8bf594c9220f2f322babeff7713b1cd356bbc5f
Murugan <s.g.murugan@accenture.com>
2023-08-28 17:19:05 +0530

    Sample Project for Jenkins build training

---
+++ JenkinsBuildProject/src/main/java/com/sampleJenkinsBuildProject/JenkinsBuildProject/App.java
@@ -0,0 +1,13 @@
+package com.sampleJenkinsBuildProject.JenkinsBuildProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
